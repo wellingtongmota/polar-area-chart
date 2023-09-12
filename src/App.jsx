a0a9a2d205791e08d3461541f4810c58c0d3f390
Wellington Galvão <wellingtongalvao96@gmail.com>
2023-09-12 10:59:23 -0300

    Criação div Polar area chart

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,39 @@
+import { Flex, Heading } from "@chakra-ui/react"
+
 const App = () => {
+
+  const qualidades = [
+    'Propriedade 1',
+    'Propriedade 2',
+    'Propriedade 3',
+    'Propriedade 4',
+    'Propriedade 5',
+  ]
+
   return (
-    <div>App</div>
+    <Flex
+      w='full'
+      h='100dvh'
+      flexDirection='column'
+      align='center'
+      bgColor='white'
+      overflowY='auto'
+      gap={2}
+    >
+
+      <Heading>Título</Heading>
+
+      <Flex 
+        bgColor='blue.50'
+        maxW='4xl'
+        w='full'
+        maxH='xl'
+        h='full'
+        p={4}
+      >
+        Polar Area Chart
+      </Flex>
+    </Flex>
   )
 }
 
